48bdac3c763644620e83a62ae427205895a93ed3
Leandro Girardelo <leandrogirardelo@gmail.com>
2017-03-04 23:51:49 -0300

    i18n

--- Gemfile
+++ Gemfile
@@ -45,8 +45,7 @@ gem 'less-rails'
 gem 'twitter-bootstrap-rails'
 gem 'font-awesome-sass', '~> 4.7.0'
 gem 'sprockets', '3.6.3'
-
-
+gem 'rails-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -7,11 +7,15 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 
+
 module RedeFeminina
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
      # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.       
+    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+ 
+    config.i18n.default_locale = 'pt-BR'
     
   end
 end
